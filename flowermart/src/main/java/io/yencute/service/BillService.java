a2bdbb12cbfd667bf94b7728539e88335377a69b
witcher-creator <dhuuloc8818@gmail.com>
2020-12-25 00:36:09 +0700

    #7: add current use to bill

--- flowermart/src/main/java/io/yencute/service/BillService.java
+++ flowermart/src/main/java/io/yencute/service/BillService.java
@@ -2,22 +2,27 @@ package io.yencute.service;
 
 import io.yencute.domain.Bill;
 import io.yencute.domain.BillItem;
+import io.yencute.domain.User;
 import io.yencute.repository.BillItemRepository;
 import io.yencute.repository.BillRepository;
+import io.yencute.repository.UserRepository;
 import io.yencute.security.SecurityUtils;
 import io.yencute.service.dto.BillDTO;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BillService {
     private final BillItemRepository billItemRepository;
     private final BillRepository billRepository;
+    private final UserRepository userRepository;
 
-    public BillService(BillItemRepository billItemRepository, BillRepository billRepository){
+    public BillService(BillItemRepository billItemRepository, BillRepository billRepository, UserRepository userRepository){
         this.billItemRepository = billItemRepository;
         this.billRepository = billRepository;
+        this.userRepository = userRepository;
     }
 
     /**
@@ -28,6 +33,9 @@ public class BillService {
         Bill bill = new Bill();
         bill.setStatus(billDTO.getStatus());
         bill.setPlacedDate(billDTO.getPlacedDate());
+        Optional<User> userOptional = userRepository.findOneByLogin(SecurityUtils.getCurrentUserLogin().orElse(null));
+        if(userOptional.isPresent())
+            bill.setUser(userOptional.get());
         Bill curBill = billRepository.save(bill);
 
         List<BillItem> billItems = billDTO.getBillItems();
